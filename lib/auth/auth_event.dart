9a2cd095e567ad3936a3f91b599bc9824f8c82ee
lulultfh <punyalulu05@gmail.com>
2026-04-04 06:38:27 +0700

    feat: menambahkan class RegisterSubmitted

--- lib/auth/auth_event.dart
+++ lib/auth/auth_event.dart
@@ -13,3 +13,18 @@ class LoginSubmitted extends AuthEvent {
   @override
   List<Object?> get props => [email, password]; 
 }
+
+class RegisterSubmitted extends AuthEvent{
+  final String name;
+  final String email;
+  final String password;
+
+  RegisterSubmitted({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+  @override
+  List<Object?> get props => [name, email, password];
+}
+
