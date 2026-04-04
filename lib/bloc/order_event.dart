06a1106166340fbe5802afd9d055d1256b09b215
lulultfh <punyalulu05@gmail.com>
2026-04-04 07:19:54 +0700

    feat: menambahkan class OrderSubmitted

--- lib/bloc/order_event.dart
+++ lib/bloc/order_event.dart
@@ -4,3 +4,21 @@ abstract class OrderEvent extends Equatable {
   @override
   List<Object?> get props => [];
 }
+
+class OrderSubmitted extends OrderEvent{
+  final String makanan;
+  final String minuman;
+  final int jumlahMakanan;
+  final int jumlahMinuman;
+
+  OrderSubmitted({
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+  });
+
+  @override
+  List<Object?> get props => [makanan, minuman, jumlahMakanan, jumlahMinuman];
+}
+
